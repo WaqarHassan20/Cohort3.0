b572d0064a58fff501f601396d32c7b8f95c6d5c
WaqarHassan20 <waqarkhalid2024@gmail.com>
2024-12-29 00:45:28 +0500

    CallBacks Hell, Promise Chain and Async Await

---
+++ Week_02/01_JavaScript/index.js
@@ -0,0 +1,24 @@
+// ==========function 1===============
+function sum(a, b) {
+  return a + b;
+}
+
+let result = sum(3, 5);
+console.log("The sum of two numbers is = ", result);
+
+let result2 = sum(10, "Hello");
+console.log("The sum of two strings is = ", result2);
+
+// ==========function 2==============//
+
+function sum(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+let totalSum = sum(10);
+console.log("The sum of all numbers upto given number is =  ", totalSum);
+
+
