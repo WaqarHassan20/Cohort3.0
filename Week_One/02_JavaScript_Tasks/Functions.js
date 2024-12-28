a09ee289ea0566db0f9bc6a212020e44f79bfdbd
WaqarHassan20 <waqarkhalid2024@gmail.com>
2024-12-28 17:05:57 +0500

    Introduction to HTML, CSS and basic JS

---
+++ Week_One/02_JavaScript_Tasks/Functions.js
@@ -0,0 +1,23 @@
+// ==========function 1===============
+function sum(str1, str2) {
+  return str1 + str2;
+}
+
+let result = sum(3, 5);
+console.log("The sum of two numbers is = ", result);
+
+let result2 = sum("he", "llo");
+console.log("The sum of two strings is = ", result2);
+
+// ==========function 2===============
+
+function canVote(age) {
+  return (result = age > 18);
+}
+
+let result3 = canVote(20);
+console.log("The result of voting for pubert is = ", result3);
+
+let result4 = canVote(15);
+console.log("The result of voting for unpubert is = ", result4);
+
