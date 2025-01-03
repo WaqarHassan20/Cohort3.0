58845b163636716e2d72bb132560650a57c22630
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-03 15:05:44 +0500

    Introduction to  middlewares and their UseCase

--- Week_04/02_http-Servers/01_Offline-express-middleware/02_middlewares/index.js
+++ Week_04/02_http-Servers/01_Offline-express-middleware/02_middlewares/index.js
@@ -11,25 +11,13 @@
 //3. End the request-response cycle
 //4. Call the next middleware function in the stack
 
-const express = require("express");
-const app = express();
-
-//function to apply middlewares
-function isOldEnough(age) {
-    if (age >= 14) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 //Using a middleware but the route is only used to show the final endpoint like what the user want to do.
 
 // app.get("/ride1",(req,res)=>{
-    //     if(isOldEnough(req.query.age)){
-        //         res.json({
-            //             msg: "you have successfully riden the ride1",
-            //         })
+//     if(isOldEnough(req.query.age)){
+//         res.json({
+//             msg: "you have successfully riden the ride1",
+//         })
 //     }else{
 //         res.status(411).json({
 //             msg:"You are not eligible for that ride yet",
@@ -37,26 +25,29 @@ function isOldEnough(age) {
 //     }
 // })
 
+//Like here user only need to ride the ride1 So everything will be checked before the route in the middlware
+// app.get("/ride1",isOldEnoughMiddlware,(req,res)=>{
+//     res.json({
+//         msg: "you have successfully riden the ride1",
+//     })
+// })
+// If we want that all the routes in the app uses this middleware then we can use app.use() and orders matter here.
+
+const express = require("express");
+const app = express();
+
 function isOldEnoughMiddlware(req, res, next) {
-    //As we know that middlware function has access to the request and response object and next function so;
-    const age = req.query.age;
-    if (age >= 14) {
-        // next() is a function in Express that is used to pass control to the next middleware function. If we don't call next() then the request will be stuck at this middleware and the code after this middleware will not be executed.
-        next();
-    } else {
-        res.json({
+  //As we know that middlware function has access to the request and response object and next function so;
+  const age = req.query.age;
+  if (age >= 14) {
+    // next() is a function in Express that is used to pass control to the next middleware function. If we don't call next() then the request will be stuck at this middleware and the code after this middleware will not be executed.
+    next();
+  } else {
+    res.json({
       msg: "Sorry, you are not eligible for that ride yet",
     });
   }
 }
-
-//Like here user only need to ride the ride1 So everything will be checked before the route in the middlware
-// app.get("/ride1",isOldEnoughMiddlware,(req,res)=>{
-    //     res.json({
-        //         msg: "you have successfully riden the ride1",
-        //     })
-        // })
-        // If we want that all the routes in the app uses this middleware then we can use app.use() and orders matter here.
 
 app.use(isOldEnoughMiddlware);
 
@@ -73,5 +64,5 @@ app.get("/ride2", (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
