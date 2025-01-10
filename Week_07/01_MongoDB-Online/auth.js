5c6fd67e50aa04edb5ad9f90b3db3d6105ebfa01
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-10 20:30:13 +0500

    Adding a separate file of authentication with jwt

---
+++ Week_07/01_MongoDB-Online/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "BappaStunning30SaalStillRunning";
+
+function auth(req, res, next) {
+  const token = req.headers.token;
+  const decodedData = jwt.verify(token, JWT_SECRET);
+
+  if (decodedData) {
+    req.userId == decodedData.id;
+    next();
+  } else {
+    res.status(403).json({ message: "Incorrect Credential Values" });
+  }
+}
+
+module.exports = {
+  auth,
+  JWT_SECRET,
+};
