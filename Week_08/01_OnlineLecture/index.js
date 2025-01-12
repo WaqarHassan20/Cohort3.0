e66343bc2c1534418eec397bef3137ad9643c202
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-13 01:33:09 +0500

    Basic code how to create a server

---
+++ Week_08/01_OnlineLecture/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin", adminRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running on the port 3000");
+});
