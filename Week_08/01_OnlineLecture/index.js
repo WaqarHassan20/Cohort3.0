c97ca2ccfda45b4e72fd2f0595289dde89df95d6
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-14 14:06:05 +0500

    Creating a server and required routes using Router

--- Week_08/01_OnlineLecture/index.js
+++ Week_08/01_OnlineLecture/index.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const { DATABASE_CONNECTION } = require("./imports");
+
 const app = express();
 
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 
+app.use(express.json());
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
 
 (async () => {
-  await mongoose.connect(
-    "mongodb+srv://waqarhassan7661:MONGODBid786@cluster0.k8erd.mongodb.net/Course-Selling-App"
-  );
+  await mongoose.connect(DATABASE_CONNECTION);
   console.log("DataBase is connected successfully");
   app.listen(3000, () => {
     console.log("Server is running on the port 3000");
