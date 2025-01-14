7879b96bcc845d4aba5f0d3c4fb468b9dd6d0bea
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-14 14:07:30 +0500

    Separate routes of User

--- Week_08/01_OnlineLecture/routes/user.js
+++ Week_08/01_OnlineLecture/routes/user.js
@@ -1,23 +1,41 @@
 const { Router } = require("express");
 const userRouter = Router();
 const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const { JWT_USER_SECRET } = require("../imports");
 
 userRouter.post("/signup", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const name = req.body.name;
+  const { email, password, firstName, lastName } = req.body;
 
   await userModel.create({
-    email: email,
-    password: hashedPassword,
-    name: name,
+    email,
+    password,
+    firstName,
+    lastName,
+  });
+
+  res.json({
+    message: "You are successfully Signed-Up as USER",
   });
 });
 
-userRouter.post("/login", (req, res) => {
-  res.json({
-    message: "User Login Route",
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  await userModel.findOne({
+    email,
+    password,
   });
+
+  if (user) {
+    const token = jwt.sign({ id: user._id }, JWT_USER_SECRET);
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).send({
+      message: "Incorrect Credentials",
+    });
+  }
 });
 
 userRouter.get("/purchases", (req, res) => {
