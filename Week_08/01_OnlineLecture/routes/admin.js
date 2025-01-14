2dd2c82fdfb6e5c0582e12dc6fc7589ef02364b2
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-14 14:07:09 +0500

    Separate routes of admin

--- Week_08/01_OnlineLecture/routes/admin.js
+++ Week_08/01_OnlineLecture/routes/admin.js
@@ -1,21 +1,63 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
+const { JWT_ADMIN_SECRET, adminMiddleWare } = require("../imports");
+const jwt = require("jsonwebtoken");
 
-adminRouter.get("/signup",(req, res) => {
-  
+adminRouter.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
 
-});
-
-adminRouter.get("/login", (req, res) => {
-  res.json({
-    message: "Admin login Route",
+  await adminModel.create({
+    email,
+    password,
+    firstName,
+    lastName,
   });
+
+  res.json("You are successfully Signed-Up as ADMIN");
 });
 
-adminRouter.get("/createCourse", (req, res) => {
+adminRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const admin = await adminModel.findOne({
+    email,
+    password,
+  });
+
+  if (!admin) {
+    res.json({
+      message: "You does not exist in the DataBase",
+    });
+    return;
+  }
+
+  if (admin) {
+    const token = jwt.sign({ id: admin_id }, JWT_ADMIN_SECRET);
+    res.status(200).send({
+      token: token,
+    });
+  } else {
+    res.status(403).send({
+      message: "Incorrect Credentials",
+    });
+  }
+});
+
+adminRouter.get("/createCourse", adminMiddleWare, async (req, res) => {
+  const { title, description, price, imageURL, creatorId } = req.body;
+
+  const course = await adminModel.create({
+    title,
+    description,
+    price,
+    imageURL,
+    creatorId,
+  });
+
   res.json({
-    message: "Admin createCourse Route",
+    message: "Course is created Successfully",
+    courseId: course.id,
   });
 });
 
