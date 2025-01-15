de59aca6dd52408b059b60ac39c55fbaa2909362
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-15 15:02:02 +0500

    Separate route for Courses

--- Week_08/01_OnlineLecture/routes/course.js
+++ Week_08/01_OnlineLecture/routes/course.js
@@ -1,15 +1,36 @@
 const { Router } = require("express");
 const courseRouter = Router();
 const { courseModel } = require("../db");
+const { userMiddleWare } = require("../middlewares/userMiddleware");
 
-courseRouter.post("/purchase", async (req, res) => {
+courseRouter.post("/purchase", userMiddleWare, async (req, res) => {
+  const uesrId = req.uesrId;
+  const courseId = req.body.courseId;
+
+  const purchases = await purchaseModel.create({
+    uesrId,
+    courseId,
+  });
+
+  const courseData = await courseModel.find({
+    _id: {
+      $in: purchases.map((p) => p.courseId),
+    },
+  });
+
   res.json({
-    message: "Course Purchase Route",
+    message: "Course Purchased Successfully",
+    purchases,
+    courseData,
   });
 });
+
 courseRouter.get("/preview", async (req, res) => {
+  const courses = await courseModel.find({});
+
   res.json({
     message: "Course Preivew Route",
+    courses,
   });
 });
 
