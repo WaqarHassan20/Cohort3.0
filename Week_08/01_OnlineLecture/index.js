08ada38fd90f82a8101444020ea177230689ee83
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-15 14:58:41 +0500

    Updated the main file

--- Week_08/01_OnlineLecture/index.js
+++ Week_08/01_OnlineLecture/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const { DATABASE_CONNECTION } = require("./imports");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,13 +11,19 @@ const { adminRouter } = require("./routes/admin");
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
+//if any request will come at /user route then it will get routed to userRouter and will implement the code present in the user.js
+
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
 
 (async () => {
-  await mongoose.connect(DATABASE_CONNECTION);
-  console.log("DataBase is connected successfully");
-  app.listen(3000, () => {
-    console.log("Server is running on the port 3000");
-  });
+  try {
+    await mongoose.connect(process.env.DATABASE_CONNECTION);
+    console.log("DataBase is connected successfully");
+    app.listen(3000, () => {
+      console.log("Server is running on the port 3000");
+    });
+  } catch (error) {
+    console.log("Error : ", error);
+  }
 })();
