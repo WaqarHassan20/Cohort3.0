d99aa6a1732344399a160e27da445f84389dbbee
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-15 15:00:02 +0500

    Separate middleware for User

--- Week_08/01_OnlineLecture/middlewares/userMiddleware.js
+++ Week_08/01_OnlineLecture/middlewares/userMiddleware.js
@@ -1,9 +1,8 @@
 const jwt = require("jsonwebtoken");
-const { JWT_USER_SECRET } = require("../imports");
 
 function userMiddleWare(req, res, next) {
   const token = req.headers.token;
-  const decodedData = jwt.verify(token, JWT_USER_SECRET);
+  const decodedData = jwt.verify(token, process.env.JWT_USER_SECRET);
 
   if (decodedData) {
     req.userId == decodedData.id;
