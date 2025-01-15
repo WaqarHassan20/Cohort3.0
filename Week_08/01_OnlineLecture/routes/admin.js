70d65aa9711540360fc6123b2797a9b2e5881415
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-15 15:00:34 +0500

    Separate routes of admin

--- Week_08/01_OnlineLecture/routes/admin.js
+++ Week_08/01_OnlineLecture/routes/admin.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
-const { JWT_ADMIN_SECRET, adminMiddleWare } = require("../imports");
+const { adminMiddleWare } = require("../middlewares/adminMiddleware");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 adminRouter.post("/signup", async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
@@ -33,7 +34,7 @@ adminRouter.post("/login", async (req, res) => {
   }
 
   if (admin) {
-    const token = jwt.sign({ id: admin_id }, JWT_ADMIN_SECRET);
+    const token = jwt.sign({ id: admin_id }, process.env.JWT_ADMIN_SECRET);
     res.status(200).send({
       token: token,
     });
@@ -45,14 +46,18 @@ adminRouter.post("/login", async (req, res) => {
 });
 
 adminRouter.get("/createCourse", adminMiddleWare, async (req, res) => {
-  const { title, description, price, imageURL, creatorId } = req.body;
+  const userId = req.userId;
+  // this user id comes from the middleware as it executes the next function
+  // and received at the request of the get functoin of Fcreate course
+
+  const { title, description, price, imageURL } = req.body;
 
   const course = await adminModel.create({
     title,
     description,
     price,
     imageURL,
-    creatorId,
+    userId,
   });
 
   res.json({
@@ -61,15 +66,43 @@ adminRouter.get("/createCourse", adminMiddleWare, async (req, res) => {
   });
 });
 
-adminRouter.get("/deleteCourse", (req, res) => {
+adminRouter.put("/addCourse", adminMiddleWare, async (req, res) => {
+  const adminId = req.adminId;
+  const { title, description, price, imageURL, courseId } = req.body;
+  const course = adminModel.findOneAndUpdate(
+    {
+      _id: courseId,
+      creatorId: adminId,
+    },
+    {
+      title,
+      description,
+      price,
+      imageURL,
+    }
+  );
+  if (!course) {
+    res.json({
+      message: "Course not found",
+    });
+  }
+
   res.json({
-    message: "Admin deleteCourse Route",
+    message: "Admin course Updated ",
+    courseId,
+    creatorId,
   });
 });
 
-adminRouter.get("/addCourse", (req, res) => {
+adminRouter.get("seeAllCourses", async (req, res) => {
+  const adminId = req.adminId;
+  const courses = await adminModel.find({
+    adminId,
+  });
+
   res.json({
-    message: "Admin addCourse Route",
+    message: "All courses founded",
+    course: courses,
   });
 });
 
