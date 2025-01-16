703edb540fd9efe1190bbed860871b8a7e71c175
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-16 18:29:49 +0500

    universal middleware for both user and admin

--- Week_08/02_PracticeProject/middlewares/authentication.js
+++ Week_08/02_PracticeProject/middlewares/authentication.js
@@ -1,41 +1,41 @@
-// Two in One function
+// // Two in One function
 
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-function authMiddleware(role, secretKey) {
-  return (req, res, next) => {
-    const token = req.headers.token;
+// function authMiddleware(role, secretKey) {
+//   return (req, res, next) => {
+//     const token = req.headers.token;
 
-    if (!token) {
-      return res.status(403).send({
-        message: "Token is missing",
-      });
-    }
+//     if (!token) {
+//       return res.status(403).send({
+//         message: "Token is missing",
+//       });
+//     }
 
-    try {
-      const decoded = jwt.verify(token, secretKey);
+//     try {
+//       const decoded = jwt.verify(token, secretKey);
 
-      if (decoded) {
-        if (role === "admin") {
-          req.adminId = decoded.id;
-        } else if (role === "user") {
-          req.userId = decoded.id;
-        }
+//       if (decoded) {
+//         if (role === "admin") {
+//           req.adminId = decoded.id;
+//         } else if (role === "user") {
+//           req.userId = decoded.id;
+//         }
 
-        next();
-      } else {
-        res.status(403).send({
-          message: `You are not an authorized ${role.toUpperCase()}`,
-        });
-      }
-    } catch (error) {
-      res.status(403).send({
-        Error: error,
-      });
-    }
-  };
-}
+//         next();
+//       } else {
+//         res.status(403).send({
+//           message: `You are not an authorized ${role.toUpperCase()}`,
+//         });
+//       }
+//     } catch (error) {
+//       res.status(403).send({
+//         Error: error,
+//       });
+//     }
+//   };
+// }
 
-module.exports = {
-  authMiddleware: authMiddleware,
-};
+// module.exports = {
+//   authMiddleware: authMiddleware,
+// };
