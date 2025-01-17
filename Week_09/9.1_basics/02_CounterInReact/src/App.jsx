d139a14ceef79c0ed2131928b66933475069761a
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-18 03:52:17 +0500

    Updated the Counter App in React

--- Week_09/9.1_basics/02_CounterInReact/src/App.jsx
+++ Week_09/9.1_basics/02_CounterInReact/src/App.jsx
@@ -6,10 +6,18 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  function increaseCount() { setCount(count + 1) }
+  function decreaseCount() {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+  function resetCount() { setCount(0) }
+
   return (
     <>
       <div>
-        <h1>Basic Boiler Code By Default</h1>
+        <h1>Default Boiler Code</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,11 +25,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>{count}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
+        <button style={{ padding: "10px 25px", margin: "15px" }} onClick={() => increaseCount()}>
+          Increase
         </button>
-      </div>
+        <button style={{ padding: "10px 25px", margin: "15px" }} onClick={() => decreaseCount()}>
+          Decrease
+        </button>
+        <button style={{ padding: "10px 25px", margin: "15px" }} onClick={() => resetCount()}>
+          Reset
+        </button>
+      </div >
     </>
   );
 }
