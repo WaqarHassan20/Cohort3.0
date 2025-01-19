41591e3e66d9f2fe2ebae8c4dfe663fcae447248
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-20 00:33:20 +0500

    Recaping the whole react concepts

---
+++ Week_09/9.3_Recap-Offline/src/App.jsx
@@ -0,0 +1,19 @@
+import StaticComponent from "./components/StaticComponent"
+import ToggleMessage from "./components/ToggleMessage"
+import Addcomponent from "./components/Addcomponent"
+import BellIcon from "./components/BellIcon"
+
+function App() {
+
+  return (
+    <>
+      <BellIcon />
+      <Addcomponent />
+      <ToggleMessage />
+      <StaticComponent />
+    </>
+  )
+}
+
+export default App
+
