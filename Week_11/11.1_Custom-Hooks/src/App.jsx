0b58be601865665c86c73a9021c1a6c985f0948d
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-24 19:15:33 +0500

    App commit

--- Week_11/11.1_Custom-Hooks/src/App.jsx
+++ Week_11/11.1_Custom-Hooks/src/App.jsx
@@ -1,8 +1,10 @@
 import Counter from "./components/CounterHook/Counter";
+import Fetch from "./components/UseFetch/Fetch";
 
 function App() {
   return (
     <>
+      <Fetch />
       <Counter />
     </>
   );
