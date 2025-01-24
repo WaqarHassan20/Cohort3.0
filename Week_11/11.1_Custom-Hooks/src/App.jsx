881cf7886c7f2e055cc97b1da4eead2be65cf602
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-25 01:34:55 +0500

    Updating the React Project

--- Week_11/11.1_Custom-Hooks/src/App.jsx
+++ Week_11/11.1_Custom-Hooks/src/App.jsx
@@ -1,9 +1,13 @@
-import Counter from "./components/CounterHook/Counter";
+import PrevValue from "./components/UsePrevHook/PrevValue";
 import Fetch from "./components/UseFetch/Fetch";
+import Counter from "./components/CounterHook/Counter";
+import Debounce from "./components/DebounceHook/Debounce";
 
 function App() {
   return (
     <>
+      <Debounce />
+      <PrevValue />
       <Fetch />
       <Counter />
     </>
