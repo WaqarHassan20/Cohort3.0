515f96f33e33c9190e96dbe476c85a8ce20a7bb2
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-28 22:24:25 +0500

    Making the Clone of Vimal Pan Masala

---
+++ Week_12/02_VimalPan-Project/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="max-w-7xl mx-auto p-4 flex items-center justify-between bg-yellow-400">
+        <h1 className="text-2xl font-extrabold tracking-tighter text-orange-600 hover:text-orange-700 transition-all duration-300">
+          Vimal
+        </h1>
+        <div className="flex gap-6 items-center">
+          <h1 className="text-xl font-medium tracking-tighter text-black hover:text-orange-900 transition-all duration-300">
+            Products
+          </h1>
+          <button className="bg-orange-600 rounded-lg px-4 py-3 font-bold text-white hover:bg-orange-700 transition-all duration-300">
+            Login
+          </button>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
+
+// border-2 border-red-600
