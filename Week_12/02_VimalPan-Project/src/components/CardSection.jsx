4c1a7742b58e858ef9a12c2124d4858e2ad8e06a
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-01-28 22:20:58 +0500

    Making the Clone of Vimal Pan Masala

---
+++ Week_12/02_VimalPan-Project/src/components/CardSection.jsx
@@ -0,0 +1,16 @@
+import Card from "./Card";
+
+function Content() {
+  return (
+    <>
+      <section className="bg-green-300 ">
+        <div className="bg-pink-300">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+    </>
+  );
+}
+export default Content;
