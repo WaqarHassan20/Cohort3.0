4490ff3237936d010fc8cc9fc1fa98c712485e96
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-10 04:11:29 +0500

    Updating the Button Component

--- Week_15/03_UI-Library-Button/01_BrainlyFrontend/src/App.tsx
+++ Week_15/03_UI-Library-Button/01_BrainlyFrontend/src/App.tsx
@@ -1,30 +1,69 @@
 import { PlusIcon } from "./components/icons/PlusIcon";
+import { ShareIcon } from "./components/icons/ShareIcon";
 import { Button } from "./components/ui/Button";
 
 function App() {
   return (
     <>
-      <Button
-        variant="primary"
-        size="sm"
-        onClick={() => {}}
-        text="Add"
-        startIcon={PlusIcon}
-      />
-      <Button
-        variant="secondary"
-        size="lg"
-        onClick={() => {}}
-        text="Share"
-        startIcon=""
-      />
-      <Button
-        variant="primary"
-        size="md"
-        onClick={() => {}}
-        text="Share"
-        startIcon=""
-      />
+      <div className="flex flex-col items-center justify-center bg-red-800 w-screen h-screen">
+        <div className="flex gap-10 items-center justify-center">
+          <div>
+            <h1 className="text-white text-xl font-bold py-4">Small Buttons</h1>
+            <Button
+              variant="primary"
+              size="sm"
+              onClick={() => {}}
+              text="Add"
+              startIcon={<PlusIcon size="sm" />}
+            />
+            <Button
+              variant="primary"
+              size="sm"
+              onClick={() => {}}
+              text="Add"
+              startIcon={<ShareIcon size="sm" />}
+            />
+          </div>
+          <div>
+            <h1 className="text-white text-4xl font-bold py-4">
+              Large Buttons
+            </h1>
+            <Button
+              variant="secondary"
+              size="lg"
+              onClick={() => {}}
+              text="Share"
+              startIcon={<PlusIcon size="lg" />}
+            />
+            <Button
+              variant="secondary"
+              size="lg"
+              onClick={() => {}}
+              text="Share"
+              startIcon={<ShareIcon size="lg" />}
+            />
+          </div>
+          <div>
+            <h1 className="text-white text-2xl font-bold py-4">
+              Medium Buttons
+            </h1>
+            <Button
+              variant="primary"
+              size="md"
+              onClick={() => {}}
+              text="Share"
+              startIcon={<PlusIcon size="md" />}
+            />
+            <Button
+              variant="primary"
+              size="md"
+              onClick={() => {}}
+              text="Share"
+              startIcon={<ShareIcon size="md" />}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
