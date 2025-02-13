a1b515ec4a2685c44bb2fe8a12ae32866b8d53cc
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-13 05:33:46 +0500

    Second Brain App in TypeScript

---
+++ Week_15/03_SecondBrainTS/01_Backend/source/database/db.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Types, model } from "mongoose";
+
+const contentTypes = ["images", "videos", "articles", "audio"];
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+const ContentSchema = new Schema({
+  link: { type: String, required: true },
+  type: { type: String, enum: contentTypes, required: true },
+  title: { type: String, required: true },
+  tags: [{ type: String, ref: "Tags", required: true }],
+  UserId: { type: Types.ObjectId, ref: "Users", required: true },
+});
+
+const LinkSchema = new Schema({
+  hash: String,
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Users",
+    required: true,
+    unique: true,
+  },
+});
+
+export const UserModel = model("Users", UserSchema);
+export const ContentModel = model("Content", ContentSchema);
+export const LinkModel = model("Links", LinkSchema);
---
+++ Week_15/03_SecondBrainTS/01_Backend/source/routes/content.ts
@@ -0,0 +1,120 @@
+import { ContentModel } from "../database/db";
+import { Router } from "express";
+const ContentRouter = Router();
+import { z } from "zod";
+import { auth } from "../middleware/auth";
+
+// Craete Content sample data
+// {
+// "link": "https://twitter.com/google/jobs",
+// "type": "videos",
+// "title": "How to get job in google as software engineer",
+// "tags": ["education", "technology", "programming"]
+// }
+
+ContentRouter.post("/", auth, async (req, res) => {
+  const requiredBody = z.object({
+    link: z.string().min(4).max(50),
+    type: z.string().min(4).max(50),
+    title: z.string().min(4).max(50),
+    tags: z.array(z.string().min(4).max(40)),
+  });
+
+  const parsedData = requiredBody.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).send({
+      message: "Invalid Input Data",
+      Error: parsedData.error,
+    });
+    return;
+  }
+  try {
+    const { link, type, title, tags } = parsedData.data;
+    const content = await ContentModel.create({
+      link: link,
+      type: type,
+      title: title,
+      tags: tags,
+      // @ts-ignore
+      UserId: req.userId,
+    });
+
+    if (content) {
+      res.status(201).send({
+        message: "Content created successfully",
+        content: content,
+      });
+    } else {
+      res.status(400).send({
+        message: "Content not created",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: "Error during content creation",
+      Error: error,
+    });
+  }
+});
+
+// To get all the content of a user pass the userId in the body
+// {
+//   "userId": "67a0999226338c82e3f01715"
+// }
+
+ContentRouter.get("/", auth, async (req, res) => {
+  const userId = (req as any).userId;
+  // console.log(userId);
+  if (!userId) {
+    res.status(400).send({ message: "UserId Error" });
+    return;
+  }
+  try {
+    const content = await ContentModel.find({ UserId: userId }).populate(
+      "_id",
+      "username"
+    );
+    console.log(content);
+    if (content) {
+      res.status(200).send({
+        message: "Content found",
+        content: content,
+      });
+    } else {
+      res.status(404).send({ message: "Content not found" });
+    }
+  } catch (error) {
+    res.status(404).send({ message: "Something went wrong", Error: error });
+  }
+});
+
+// To delete the content pass the id of the content in the body
+// {
+// "id": "67a09c3b64a99983bf425466"
+// }
+
+ContentRouter.delete("/", auth, async (req, res) => {
+  const contentId = req.body.id;
+  if (!contentId) {
+    res.status(400).send({ message: "ContentId Error" });
+    return;
+  }
+  try {
+    const deletedContent = await ContentModel.deleteOne({
+      _id: contentId,
+      //@ts-ignore
+      UserId: req.userId,
+    });
+    if (deletedContent) {
+      res.status(200).send({
+        message: "Content deleted successfully",
+      });
+    } else {
+      res.status(404).send({ message: "Content not found" });
+    }
+  } catch (error) {
+    res.status(404).send({ message: "Something went wrong", Error: error });
+  }
+});
+
+export { ContentRouter };
