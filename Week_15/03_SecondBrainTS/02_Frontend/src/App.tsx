1060b6e6d8af9c59d7fc5fa0af7d1d6724f08bbc
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-14 05:08:45 +0500

    Building frontend of Brain App

--- Week_15/03_SecondBrainTS/02_Frontend/src/App.tsx
+++ Week_15/03_SecondBrainTS/02_Frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import { Button } from "./components/Button";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Button text="Share" variant="secondary" startIcon={<ShareIcon />} />
+      <Button text="Add Icon" variant="primary" startIcon={<PlusIcon />} />
+    </>
+  );
 }
 
 export default App;
