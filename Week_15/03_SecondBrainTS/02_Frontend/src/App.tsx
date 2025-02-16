a91a397d0bfc12bed4594ddcecec0db3ddf45e75
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-17 03:53:32 +0500

    Building frontend of Brain App

--- Week_15/03_SecondBrainTS/02_Frontend/src/App.tsx
+++ Week_15/03_SecondBrainTS/02_Frontend/src/App.tsx
@@ -1,55 +1,18 @@
-import { useState } from "react";
-import { Button } from "./components/Button";
-import Card from "./components/Card";
-import CreateContentModel from "./components/CreateContentModel";
-import { PlusIcon } from "./icons/PlusIcon";
-import { ShareIcon } from "./icons/ShareIcon";
-import Sidebar from "./components/Sidebar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import { Signin } from "./pages/Signin";
 
 function App() {
-  const [modelOpen, setModelOpen] = useState(false);
-
   return (
     <>
-      <Sidebar />
-      
-      <div className="p-4 ml-72 bg-[#F8FAFC] min-h-screen">
-        <CreateContentModel
-          open={modelOpen}
-          onClose={() => {
-            setModelOpen(false);
-          }}
-        />
-        <div className="flex justify-end gap-4 mt-2 ">
-          <Button
-            onClick={() => {
-              setModelOpen(true);
-            }}
-            text="Add Icon"
-            variant="primary"
-            startIcon={<PlusIcon />}
-          />
-          <Button
-            text="Share Brain"
-            variant="secondary"
-            startIcon={<ShareIcon />}
-          />
-        </div>
-
-        <div className="flex gap-4">
-          <Card
-            title="5 Crore"
-            link="https://www.youtube.com/embed/E5zXCY63WpU?si=EKyvU4QlklhAyoq8"
-            type="youtube"
-          />
-
-          <Card
-            title="Start-Up"
-            link="https://twitter.com/im_tolumichael/status/1890414337254178851?ref_src=twsrc%5Etfw"
-            type="twitter"
-          />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
