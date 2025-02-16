c57a81c5c205fa7f0309e3cbc51b931cb76915ef
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-02-16 06:13:41 +0500

    Building frontend of Brain App

--- Week_15/03_SecondBrainTS/02_Frontend/src/App.tsx
+++ Week_15/03_SecondBrainTS/02_Frontend/src/App.tsx
@@ -1,12 +1,52 @@
+import { useState } from "react";
 import { Button } from "./components/Button";
+import Card from "./components/Card";
+import CreateContentModel from "./components/CreateContentModel";
 import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
+  const [modelOpen, setModelOpen] = useState(false);
+
   return (
     <>
-      <Button text="Share Brain" variant="secondary" startIcon={<ShareIcon />} />
-      <Button text="Add Icon" variant="primary" startIcon={<PlusIcon />} />
+      <div className="p-4">
+        <CreateContentModel
+          open={modelOpen}
+          onClose={() => {
+            setModelOpen(false);
+          }}
+        />
+        <div className="flex justify-end gap-4">
+          <Button
+            onClick={() => {
+              setModelOpen(true);
+            }}
+            text="Add Icon"
+            variant="primary"
+            startIcon={<PlusIcon />}
+          />
+          <Button
+            text="Share Brain"
+            variant="secondary"
+            startIcon={<ShareIcon />}
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <Card
+            title="5 Crore"
+            link="https://www.youtube.com/embed/E5zXCY63WpU?si=EKyvU4QlklhAyoq8"
+            type="youtube"
+          />
+
+          <Card
+            title="Start-Up"
+            link="https://twitter.com/im_tolumichael/status/1890414337254178851?ref_src=twsrc%5Etfw"
+            type="twitter"
+          />
+        </div>
+      </div>
     </>
   );
 }
