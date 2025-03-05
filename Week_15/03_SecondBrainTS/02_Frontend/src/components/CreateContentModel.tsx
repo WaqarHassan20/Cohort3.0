b772de0bfb7087290ecf8190e40efff8ba8ae03b
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-05 20:03:58 +0500

    Building frontend of Brain App

--- Week_15/03_SecondBrainTS/02_Frontend/src/components/CreateContentModel.tsx
+++ Week_15/03_SecondBrainTS/02_Frontend/src/components/CreateContentModel.tsx
@@ -13,6 +13,9 @@ import { BACKEND_URL } from "../config";
 enum ContentType {
   Youtube = "youtube",
   Twitter = "twitter",
+  Instagram = "instagram",
+  Facebook = "facebook",
+  LinkedIn = "linkedin",
 }
 
 function CreateContentModel({ open, onClose }) {
@@ -25,11 +28,20 @@ function CreateContentModel({ open, onClose }) {
     const title = titleRef.current?.value;
     const link = titleRef.current?.value;
     const tags = titleRef.current?.value;
-    await axios.post(
-      `${BACKEND_URL}/api/v1/content`,
-      { title, link, type, tags },
-      { headers: { Authorization: localStorage.getItem("token") } }
-    );
+
+    const tagsArray = tags?.split(",").map((tag) => tag.trim()) || [];
+
+    console.log(title, link, type, tagsArray);
+
+    try {
+      await axios.post(
+        `${BACKEND_URL}/api/v1/content`,
+        { title, link, type, tagsArray },
+        { headers: { Authorization: localStorage.getItem("token") } }
+      );
+    } catch (error) {
+      console.error("Error adding content:", error);
+    }
   }
 
   return (
@@ -37,7 +49,7 @@ function CreateContentModel({ open, onClose }) {
       <div>
         {open && (
           <div className="bg-slate-900 fixed top-0 left-0 w-screen h-screen opacity-98 flex justify-center items-center">
-            <span className="bg-white opacity-100 p-4 w-full max-w-sm h-3/5 rounded-lg text-lg font-bold">
+            <span className="bg-white opacity-100 p-4 w-full max-w-sm h-4/6 rounded-lg text-lg font-bold">
               <div className="flex justify-end mb-1">
                 <div onClick={onClose}>
                   <CrossIcon />
@@ -54,7 +66,7 @@ function CreateContentModel({ open, onClose }) {
                 <h1 className="font-semibold mt-7 text-xl ">
                   Select the type :
                 </h1>
-                <div className="flex gap-8 mt-2">
+                <div className="grid grid-cols-2 gap-4 mt-4">
                   <Button
                     onClick={() => {
                       setType(ContentType.Youtube);
@@ -73,10 +85,37 @@ function CreateContentModel({ open, onClose }) {
                       type === ContentType.Twitter ? "primary" : "secondary"
                     }
                   />
+                  <Button
+                    onClick={() => {
+                      setType(ContentType.LinkedIn);
+                    }}
+                    text="LinkedIn"
+                    variant={
+                      type === ContentType.LinkedIn ? "primary" : "secondary"
+                    }
+                  />
+                  <Button
+                    onClick={() => {
+                      setType(ContentType.Instagram);
+                    }}
+                    text="Instagram"
+                    variant={
+                      type === ContentType.Instagram ? "primary" : "secondary"
+                    }
+                  />
+                  <Button
+                    onClick={() => {
+                      setType(ContentType.Facebook);
+                    }}
+                    text="Facebook"
+                    variant={
+                      type === ContentType.Facebook ? "primary" : "secondary"
+                    }
+                  />
                 </div>
               </div>
 
-              <div className="flex justify-center mt-12 px-10">
+              <div className="flex justify-center mt-12 px-7">
                 <Button
                   onClick={addContent}
                   fullWidth={true}
