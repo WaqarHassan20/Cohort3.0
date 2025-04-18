904daf7d4fda21864e6fc3524a36a0e6d78545fc
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-04-18 18:13:49 +0500

    Using Prisma client for Queries

--- Week_18/18.1_Prisma-and-ORMs/dist/index.js
+++ Week_18/18.1_Prisma-and-ORMs/dist/index.js
@@ -8,9 +8,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const prisma_1 = require("../src/generated/prisma");
-const client = new prisma_1.PrismaClient();
+const client = new prisma_1.PrismaClient({
+    log: ["query"],
+});
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield client.user.findMany();
+    res.json(users);
+}));
+app.get("/user/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const user = yield client.user.findFirst({
+        where: { id: Number(id) },
+        include: { todo: true },
+    });
+    res.json({ user });
+}));
 function createUser() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.user.create({
@@ -43,6 +63,19 @@ function deleteUser() {
         });
     });
 }
+function findUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield client.user.findFirst({
+            where: { id: 2 },
+            include: { todo: true },
+        });
+        console.log(user);
+    });
+}
 // createUser();
 // updateUser();
-deleteUser();
+// deleteUser();
+// findUser();
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
--- Week_18/18.1_Prisma-and-ORMs/src/index.ts
+++ Week_18/18.1_Prisma-and-ORMs/src/index.ts
@@ -1,6 +1,28 @@
 import { PrismaClient } from "../src/generated/prisma";
 
 const client = new PrismaClient();
+// {log: ["query"],} this is optioinal to pass to see the raw query running under the hood
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await client.user.findMany();
+  res.json(users);
+});
+
+app.get("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await client.user.findFirst({
+    where: { id: Number(id) },
+    include: { todo: true },
+  });
+
+  res.json({ user });
+});
 
 async function createUser() {
   await client.user.create({
@@ -30,7 +52,19 @@ async function deleteUser() {
     where: { id: 1 },
   });
 }
+async function findUser() {
+  const user = await client.user.findFirst({
+    where: { id: 2 },
+    include: { todo: true },
+  });
+  console.log(user);
+}
 
 // createUser();
 // updateUser();
 // deleteUser();
+// findUser();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
