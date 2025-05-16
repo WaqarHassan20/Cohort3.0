e874d108341bce5785a2c40f1c70c9e282ddab9d
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-05-16 19:09:36 +0500

    create rectangle on canvas

--- W-22_Excalidraw-Project/excalidraw-app/apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
+++ W-22_Excalidraw-Project/excalidraw-app/apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -4,6 +4,6 @@ export default async function Canvas({params}: {params:{roomId:string}}){
     const roomId =(await params).roomId;
     console.log(roomId)
 
-    return <CanvasComp roomId={roomIdj} />
+    return <CanvasComp roomId={roomId} />
 
 }
