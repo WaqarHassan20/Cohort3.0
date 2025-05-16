2aa84f1de9411bb5b68bd648503bf1ccc3a76a49
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-05-16 11:41:33 +0500

    create rectangle on canvas

--- W-22_Excalidraw-Project/excalidraw-app/apps/excalidraw-frontend/draw/index.ts
+++ W-22_Excalidraw-Project/excalidraw-app/apps/excalidraw-frontend/draw/index.ts
@@ -1,38 +1,77 @@
-export function initDraw(canvas:HTMLCanvasElement) {
+type shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      radius: number;
+      centerX: number;
+      centerY: number;
+    };
 
-    const ctx = canvas.getContext("2d");
-    if (!ctx) 
-        {return}
+export function initDraw(canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "rgba(0,0,0)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  let existingShapes: shape[] = [];
 
-    let clicked = false;
-    let startX = 0;
-    let starty = 0;
+  if (!ctx) {
+    return;
+  }
 
-    canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        startX = e.clientX;
-        starty = e.clientY;
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  let clicked = false;
+  let startX = 0;
+  let starty = 0;
+
+  canvas.addEventListener("mousedown", (e) => {
+    clicked = true;
+    startX = e.clientX;
+    starty = e.clientY;
+  });
+
+  canvas.addEventListener("mouseup", (e) => {
+    clicked = false;
+    const width = e.clientX - startX;
+    const height = e.clientY - starty;
+    existingShapes.push({
+      type: "rect",
+      x: startX,
+      y: starty,
+      width,
+      height,
     });
+  });
 
-    canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        console.log(e.clientX);
-        console.log(e.clientY);
-    });
+  canvas.addEventListener("mousemove", (e) => {
+    if (clicked) {
+      const width = e.clientX - startX;
+      const height = e.clientY - starty;
 
-    canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-        const width = e.clientX - startX;
-        const height = e.clientY - starty;
+      ctx.strokeStyle = "rgba(255,255,255)";
+      ctx.strokeRect(startX, starty, width, height);
+    }
+  });
+}
 
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "rgba(0,0,0)";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeStyle = "rgba(255,255,255)";
-        ctx.strokeRect(startX, starty, width, height);
-        }
-    });
+function clearCanvas(
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+  existingShapes: shape[]
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeStyle = "rgba(255,255,255)";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+    }
+  });
 }
